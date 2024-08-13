db9ea5d73a0f56f0dcdb9f3c17b31b9623c7db60
CRISTANCHOmwa12 <phamtompertlandres@gmail.com>
2024-08-13 16:21:10 -0500

    Create inform.php

---
+++ inform.php
@@ -0,0 +1,15 @@
+<?php
+$Identificacion= $_POST ['Identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del Empleado es: ".p1;
+echo "La identificacion del Empleado es: ".p2;
+echo "La identificacion del Empleado es: ".p3;
+echo "La identificacion del Empleado es: ".pre1;
+echo "La identificacion del Empleado es: ".pre2;
+echo "La identificacion del Empleado es: ".pre2;
+echo "Los nombres y apellidos del empleado son: "NomApellidos;
+echo "El cago del empleado es: ".Cargo;
+echo "El salario del empeado es:  ".Salario;
+?>
